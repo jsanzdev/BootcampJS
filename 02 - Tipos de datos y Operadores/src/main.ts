212927a5d6ba1b711c5ffd6b37680fe0339afdb1
Jesus Sanz <jesus@jsanz.dev>
2024-03-02 17:50:45 +0100

    added types.

--- 02 - Tipos de datos y Operadores/src/main.ts
+++ 02 - Tipos de datos y Operadores/src/main.ts
@@ -2,12 +2,12 @@ import "./style.css";
 
 console.log("Hello Typescript!");
 
-let cena = 120;
-let bebidas = 18;
-let cenaSinBebidas = cena - bebidas;
+let cena:number = 120;
+let bebidas:number = 18;
+let cenaSinBebidas:number = cena - bebidas;
 
-let totalYo = (cenaSinBebidas/6) + bebidas;
-let totalAmigo = cenaSinBebidas/6;
+let totalYo:number = (cenaSinBebidas/6) + bebidas;
+let totalAmigo:number = cenaSinBebidas/6;
 
 console.log(`Total a pagar por mi: ${totalYo}`);
 console.log(`Total a pagar por cada amigo: ${totalAmigo}`);
