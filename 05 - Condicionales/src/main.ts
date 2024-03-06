c7c1ae40c27ee344009dfb4792de52b71c65b75e
Jesus Sanz <jesus@jsanz.dev>
2024-03-06 19:51:46 +0100

    Gameloop added - Added Restart, Play and Win functionallity

--- 05 - Condicionales/src/main.ts
+++ 05 - Condicionales/src/main.ts
@@ -1,69 +1,162 @@
 import "./style.css";
 
-const score: number = 0;
-
-if (score > 5) {
-  console.log("You win!");
-}
+let score: number = 0;
 
 type Cartas = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 10 | 11 | 12;
 
 function generateNumber(): number {
-    return Math.floor(Math.random() * 10) + 1;
+  return Math.floor(Math.random() * 10) + 1;
 }
 
 function showCard(card: Cartas) {
-    let cardImage:string = "";
+  let cardImage: string = "";
 
-    switch (card) {
-        case 1:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
-            break;
-        case 2:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
-            break;
-        case 3:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
-            break;
-        case 4:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
-            break;
-        case 5:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
-            break;
-        case 6:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
-            break;
-        case 7:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
-            break;
-        case 10:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
-            break;
-        case 11:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
-            break;
-        case 12:
-            cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
-            break;
-            default:
-                cardImage = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-                break;
-    }
+  switch (card) {
+    case 1:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
+      break;
+    case 2:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
+      break;
+    case 3:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
+      break;
+    case 4:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
+      break;
+    case 5:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
+      break;
+    case 6:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
+      break;
+    case 7:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+      break;
+    case 10:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+      break;
+    case 11:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+      break;
+    case 12:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+      break;
+    default:
+      cardImage =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+      break;
+  }
 
-    let cardElement = document.getElementById("card") as HTMLImageElement;
+  let cardElement = document.getElementById("card") as HTMLImageElement;
 
-    if (cardElement) {
-        cardElement.src = cardImage;
-    }
+  if (cardElement && cardElement instanceof HTMLImageElement) {
+    cardElement.src = cardImage;
+  }
 }
 
-document.getElementById("pedir-carta")?.addEventListener("click", () => {
-    let card = generateNumber();
-    if (card > 7) {
-        card += 2;
+function updateScore(newScore: number) {
+  if (newScore > 7) {
+    score += 0.5;
+  } else {
+    score += newScore;
+  }
+  let scoreElement = document.getElementById("puntuacion");
+  if (scoreElement && scoreElement instanceof HTMLSpanElement) {
+    scoreElement.innerText = score.toString();
+  }
+  if (score === 7.5) {
+    console.log("Has ganado");
+    if (botonPedirCarta && botonPedirCarta instanceof HTMLButtonElement) {
+      botonPedirCarta.disabled = true;
     }
-    console.log(card);
-    showCard(card as Cartas);
-});
+  } else if (score > 7.5) {
+    console.log("Has perdido");
+    if (botonPedirCarta && botonPedirCarta instanceof HTMLButtonElement) {
+      botonPedirCarta.disabled = true;
+    }
+  }
+}
 
+function playCard(): void {
+  let card = generateNumber();
+  if (card > 7) {
+    card += 2;
+  }
+  console.log(card);
+  showCard(card as Cartas);
+  updateScore(card);
+}
+
+// function disableButton(button: HTMLButtonElement): void {
+//   if (button && button instanceof HTMLButtonElement) {
+//     button.disabled = true;
+//   }
+// }
+
+// function enableButton(button: HTMLButtonElement): void {
+//   if (button && button instanceof HTMLButtonElement) {
+//     button.disabled = false;
+//   }
+// }
+
+function plantarse(): void {
+  if (score < 4) {
+    console.log("Has sido muy conservador");
+  } else if (score === 5) {
+    console.log("Te ha entrado el canguelo eh?");
+  } else if (score === 6 || score === 7) {
+    console.log("Casi casi...");
+  }
+  if (botonPedirCarta && botonPedirCarta instanceof HTMLButtonElement) {
+    botonPedirCarta.disabled = true;
+  }
+}
+
+function restartGame(): void {
+  score = 0;
+  let scoreElement = document.getElementById("puntuacion");
+  if (scoreElement && scoreElement instanceof HTMLSpanElement) {
+    scoreElement.innerText = score.toString();
+  }
+  let cardElement = document.getElementById("card") as HTMLImageElement;
+  if (cardElement && cardElement instanceof HTMLImageElement) {
+    cardElement.src =
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+  }
+  if (botonPedirCarta && botonPedirCarta instanceof HTMLButtonElement) {
+    botonPedirCarta.disabled = false;
+  }
+}
+
+const botonPedirCarta = document.getElementById("pedir-carta");
+const botonReiniciar = document.getElementById("reiniciar");
+const botonPlantarse = document.getElementById("plantarse");
+
+if (botonPedirCarta && botonPedirCarta instanceof HTMLButtonElement) {
+  botonPedirCarta.addEventListener("click", () => {
+    playCard();
+  });
+}
+
+if (botonPlantarse && botonPlantarse instanceof HTMLButtonElement) {
+  botonPlantarse.addEventListener("click", () => {
+    plantarse();
+  });
+}
+
+if (botonReiniciar && botonReiniciar instanceof HTMLButtonElement) {
+  botonReiniciar.addEventListener("click", () => {
+    restartGame();
+  });
+}
