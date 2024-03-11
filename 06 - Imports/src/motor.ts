f487c43dd7e7d0c578019e2df4652c2a324284f1
Jesus Sanz <jesus@jsanz.dev>
2024-03-11 20:30:12 +0100

    Refactor Module 06 - Moved all functions from main.ts

--- 06 - Imports/src/motor.ts
+++ 06 - Imports/src/motor.ts
@@ -1,3 +1,16 @@
-export function generateNumber(): number {
+import { showCard, updateScore } from "./ui";
+import { Cartas } from "./types";
+
+function generateNumber(): number {
   return Math.floor(Math.random() * 10) + 1;
+}
+
+export function playCard(): void {
+  let card = generateNumber();
+  if (card > 7) {
+    card += 2;
+  }
+  console.log(card);
+  showCard(card as Cartas);
+  updateScore(card);
 }
--- 06 - Imports/src/ui.ts
+++ 06 - Imports/src/ui.ts
@@ -107,11 +107,57 @@ export function updateScore(newScore: number) {
   }
 }
 
-export function updateMessage(message: string): void {
+function updateMessage(message: string): void {
   if (
     htmlObjects.messageElement &&
     htmlObjects.messageElement instanceof HTMLSpanElement
   ) {
     htmlObjects.messageElement.innerText = message;
+  }
+}
+
+export function restartGame(): void {
+  game.score = 0;
+
+  if (
+    htmlObjects.scoreElement &&
+    htmlObjects.scoreElement instanceof HTMLSpanElement
+  ) {
+    htmlObjects.scoreElement.innerText = game.score.toString();
+  }
+  if (
+    htmlObjects.cardElement &&
+    htmlObjects.cardElement instanceof HTMLImageElement
+  ) {
+    htmlObjects.cardElement.src =
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+  }
+  if (
+    htmlObjects.botonPedirCarta &&
+    htmlObjects.botonPedirCarta instanceof HTMLButtonElement
+  ) {
+    htmlObjects.botonPedirCarta.disabled = false;
+  }
+  if (
+    htmlObjects.messageElement &&
+    htmlObjects.messageElement instanceof HTMLSpanElement
+  ) {
+    htmlObjects.messageElement.innerText = "";
+  }
+}
+
+export function plantarse(): void {
+  if (game.score < 4) {
+    updateMessage("Has sido muy conservador");
+  } else if (game.score === 5) {
+    updateMessage("Te ha entrado el canguelo eh?");
+  } else if (game.score === 6 || game.score === 7) {
+    updateMessage("Casi casi...");
+  }
+  if (
+    htmlObjects.botonPedirCarta &&
+    htmlObjects.botonPedirCarta instanceof HTMLButtonElement
+  ) {
+    htmlObjects.botonPedirCarta.disabled = true;
   }
 }
