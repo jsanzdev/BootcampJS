f29d40bf836b240eca2fac0eab30792027d02324
Jesus Sanz <jesus@jsanz.dev>
2024-03-14 17:27:26 +0100

    Module 08.2 - Arrays - Solved the problem with array methods.

---
+++ 08.2 - Arrays/src/main.ts
@@ -0,0 +1,68 @@
+import "./style.css";
+import { pacientes, Pacientes, NumeroPacientesPorEspecialidad } from "./model";
+
+console.log(pacientes);
+
+const obtenPacientesAsignadosAPediatria = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  return pacientes.filter((paciente) => paciente.especialidad === "Pediatra");
+};
+
+console.log(obtenPacientesAsignadosAPediatria(pacientes));
+
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  let pacientesPediatria = obtenPacientesAsignadosAPediatria(pacientes);
+  return pacientesPediatria.filter((paciente) => paciente.edad < 10);
+};
+
+console.log(obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes));
+
+const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+  return pacientes.some(
+    (paciente) => paciente.frecuenciaCardiaca > 100 && paciente.temperatura > 39
+  );
+};
+
+console.log(activarProtocoloUrgencia(pacientes));
+
+const reasignaPacientesAMedicoFamilia = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  return pacientes.filter(
+    (paciente) => paciente.especialidad === "Medico de familia"
+  );
+};
+
+console.log(reasignaPacientesAMedicoFamilia(pacientes));
+
+const HayPacientesDePediatria = (pacientes: Pacientes[]): boolean => {
+  return pacientes.some((paciente) => paciente.especialidad === "Pediatra");
+};
+
+console.log(HayPacientesDePediatria(pacientes));
+
+const cuentaPacientesPorEspecialidad = (
+  pacientes: Pacientes[]
+): NumeroPacientesPorEspecialidad => {
+  let medicoDeFamilia = pacientes.filter(
+    (paciente) => paciente.especialidad === "Medico de familia"
+  ).length;
+  let pediatria = pacientes.filter(
+    (paciente) => paciente.especialidad === "Pediatra"
+  ).length;
+  let cardiologia = pacientes.filter(
+    (paciente) => paciente.especialidad === "Cardiólogo"
+  ).length;
+  const numeroPacientesPorEspecialidad: NumeroPacientesPorEspecialidad = {
+    medicoDeFamilia: medicoDeFamilia,
+    pediatria: pediatria,
+    cardiologia: cardiologia,
+  };
+
+  return numeroPacientesPorEspecialidad;
+};
+
+console.log(cuentaPacientesPorEspecialidad(pacientes));
